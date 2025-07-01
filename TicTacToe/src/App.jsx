a7a7049e17b51b99c887585de634a5e115705fed
Chino <chinomsoaugustine073@gmail.com>
2025-07-01 08:37:46 -0700

    Testing

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -4,43 +4,34 @@ import './index.css';
 
 
 
-//fun to change value fo each square
-function Square() {
-  //using useState to remember when btn is clicked
-  const [value, setValue] = useState(null);
-  function handleClick() {
-    setValue('X'); {/*Changes value to X */}
-  }
-  return (
-    <button
-      className='square'
-      onClick={handleClick}>
-      {value}
-    </button>)
-
+//fun to set the squares
+function Square({ value }) {
+  return <button className="square">{value}</button>
 }
 
-export default function Board() {
-  return (
-    <>
-      <div className='board-row'>
-        < Square />
-        < Square />
-        < Square />
-      </div>
+      export default function Board() {
+  /*Creating useStates to monitor states of each square */
+const [squares, setSquares] = useState(Array(9).fill(null)); {/*Created an array that holds 9 elem and keep them empty */}
+      return (
+      <>
+        <div className='board-row'>
+          < Square value={squares[0]} />
+          < Square value={squares[1]} />
+          < Square value={squares[2]} />
+        </div>
 
-      <div className='board-row'>
-        < Square />
-        < Square />
-        < Square />
-      </div>
+        <div className='board-row'>
+          < Square value={squares[3]} />
+          < Square value={squares[4]} />
+          < Square value={squares[5]} />
+        </div>
 
-      <div className='board-row'>
-        < Square />
-        < Square />
-        < Square />
-      </div>
-    </>
-  );
+        <div className='board-row'>
+          < Square value={squares[6]} />
+          < Square value={squares[7]} />
+          < Square value={squares[8]} />
+        </div>
+      </>
+      );
 }
 
