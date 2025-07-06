b199bfce14822fce0fcdf5d4f80015501fb0ce16
Chino <chinomsoaugustine073@gmail.com>
2025-07-05 22:16:39 -0700

    Old

---
+++ learn/React/Data and States/Hooks/useReducer/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Exam1 from './Exam1'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+       <div> 
+       <Exam1 />
+       </div>
+    </div>
+     
+    
+  )
+}
+
+export default App
---
+++ learn/React/Data and States/Hooks/useReducer/src/Exam1.tsx
@@ -0,0 +1,36 @@
+import { useReducer } from "react";
+
+{/*useReducer practice 1: 
+    building counter app      
+*/}
+
+{/*Creating types for state and action which will be automatically inferred in the code
+    starts with interface State{types of states to use ex string, number, bools etx }*/}
+
+{/*states will havre count which is a number and error handler which is a string or sometimes null */}
+interface State{
+    count: Number; 
+    error: string | null; 
+}
+
+{/*Two main actions to perform: increment and decrement */}
+interface Action{
+    type: "Increment" | "Decrement"; 
+}
+
+{/*Function for reducer: takes 2 args, type state which is State and action which is Action*/}
+function reducer(state: State, action: Action ){
+
+}
+
+function Demo() {
+    {/*Creating state object: similar to useState except require 2 args - actions to perform and initial value of state
+        Depending on the type of actions perform, state will be updated  
+    */}
+    const [state, dispatch] = useReducer();
+
+    return <div className="Practice"></div>
+}
+
+
+export default Demo; 
